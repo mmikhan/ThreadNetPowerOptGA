70ce1971df3ebf5a00f875a759507d64c68b04ee
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2023-01-13 23:53:15 +0100

    Initialize code

---
+++ ga/optimization.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("This is the genetic algorithm to optimize the transmission power for Thread network.")
---
+++ model/network.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("This is the initial program to bootstrap the mathematical model.")
